f617844394be98aa4027640655bcd34485bf6060
deepak <deepak@DELL>
2018-05-12 10:36:49 +0530

    new version eee

---
+++ GitProject/src/FirstGitProject.java
@@ -0,0 +1,10 @@
+
+public class FirstGitProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to git");
+
+	}
+
+}
---
+++ GitProjectNew/src/Gitproject1.java
@@ -0,0 +1,9 @@
+
+public class Gitproject1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
