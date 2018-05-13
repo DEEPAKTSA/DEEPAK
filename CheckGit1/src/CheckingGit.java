060a8840ec7cb10286177df317faaaeb4c05e302
deepak <deepak@DELL>
2018-05-13 11:26:07 +0530

    update1

--- CheckGit1/src/CheckingGit.java
+++ CheckGit1/src/CheckingGit.java
@@ -4,6 +4,7 @@ public class CheckingGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("checking the git");
+		System.out.println("ethellam oru doubt ah");
 
 
 	}
