84c24d61c7c4ea58767f442940dc31f5346b5d02
deepak <deepak@DELL>
2018-05-13 10:17:13 +0530

    autotrigger

--- CheckGit/src/CheckingGit.java
+++ CheckGit/src/CheckingGit.java
@@ -3,7 +3,7 @@ public class CheckingGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("checking the git");
+System.out.println("hello this is git");
 	}
 
 }
